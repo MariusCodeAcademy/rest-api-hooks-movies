42552878ad9359efa67d2e21cb6cffef592e1253
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-18 11:58:00 +0300

    showing components according to logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,13 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
-  /*
-  {
-    created: "2014-12-10T14:23:31.880000Z"
-    director: "George Lucas"
-    edited: "2014-12-20T19:49:45.256000Z"
-    episode_id: 4
-    opening_crawl: "It is a 
-    release_date: "1977-05-25"
-    title: "A New Hope"
-  }
-  */
-
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const { data } = await axios.get("https://swapi.dev/api/films");
-    console.log(data.results);
+
     // perdaryti duomenis i mums rekalingastruktura
     const moviesTransformed = data.results.map((mv) => {
       return {
@@ -45,15 +21,20 @@ function App() {
       };
     });
     setMovies(moviesTransformed);
+    setIsLoading(false);
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button disabled={isLoading} onClick={fetchMoviesHandler}>
+          {isLoading ? "Loading" : "Fetch Movies"}
+        </button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies at the moment </p>}
+        {isLoading && <p>Loading ...</p>}
       </section>
     </React.Fragment>
   );
